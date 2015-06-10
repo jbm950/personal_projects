22b71f8b3438d9b06d6223fc66ce9cba84fbdc91
jbm950 <jmilam343@gmail.com>
2015-06-10 18:53:28 -0500

    made move around tiles pep8 compliant

--- General Python/Move around tiles/move_around_tiles.py
+++ General Python/Move around tiles/move_around_tiles.py
@@ -16,10 +16,11 @@ import pygame_toolbox.graphics.widgets as ptgw
 import pygame_toolbox.tilegame_tools as pttt
 
 pygame.init()
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
+
 
 class monster(object):
-    def __init__(self,monsternum):
+    def __init__(self, monsternum):
         # Open up the text file with the monster data and prepare a list of the
         # different lines of data
         self.rawdata = open('monsterdata.txt')
@@ -27,15 +28,17 @@ class monster(object):
 
         # Take the raw data and set the monsters attributes
         self.name = self.rawdata[0]
-        self.tile = ptgw.wTile(self.name, self.rawdata[1],(150,150))
-        self.tile.initialize_shade('black',(0,0,0),235)
+        self.tile = ptgw.wTile(self.name, self.rawdata[1], (150, 150))
+        self.tile.initialize_shade('black', (0, 0, 0), 235)
         self.hp = int(self.rawdata[2])
         self.damage = int(self.rawdata[3])
         self.hpmax = int(self.rawdata[4])
 
+
 class army(list):
-    def shade_monster(self,monster,color):
+    def shade_monster(self, monster, color):
         self[monster].tile.toggle_shade(color)
+
     def check_defeat(self):
         # 0 means still have a living monster
         for i in self:
@@ -43,33 +46,34 @@ class army(list):
                 return 0
         return 1
 
+
 class Tile(pttt.Tile):
     def __init__(self):
-        pttt.Tile.__init__(self,"Forrest.png",(200,150))
+        pttt.Tile.__init__(self, "Forrest.png", (200, 150))
         self.army = None
-        self.armyflag = ptg.Button(1,'flag.png',(0,0),resize = (200,150))
-        self.armyflag.image.set_colorkey((255,255,255))
+        self.armyflag = ptg.Button(1, 'flag.png', (0, 0), resize=(200, 150))
+        self.armyflag.image.set_colorkey((255, 255, 255))
 
-    def initialize_army(self,army):
+    def initialize_army(self, army):
         self.army = army
         self.image.blit(*self.armyflag.blitinfo)
 
     def remove_army(self):
         self.army = None
-        self.image.blit(self.pic,(0,0))
+        self.image.blit(self.pic, (0, 0))
 
-    def toggle_shade(self,shade):
-        pttt.Tile.toggle_shade(self,shade)
+    def toggle_shade(self, shade):
+        pttt.Tile.toggle_shade(self, shade)
         if self.army:
             self.initialize_army(self.army)
 
 
 class Tilemap(pttt.Tilemap):
-    def __init__(self,tilelist):
-        pttt.Tilemap.__init__(self,(800,600),tilelist,1)
+    def __init__(self, tilelist):
+        pttt.Tilemap.__init__(self, (800, 600), tilelist, 1)
 
-    def move_update(self,tileclicked,screen,clock):
-        adj_tiles = self.tilelist.adjacent_tiles(tileclicked,'p')
+    def move_update(self, tileclicked, screen, clock):
+        adj_tiles = self.tilelist.adjacent_tiles(tileclicked, 'p')
         army_present = []
         for i in adj_tiles:
             if i.army is None:
@@ -82,12 +86,13 @@ class Tilemap(pttt.Tilemap):
         self.__init__(self.tilelist)
         waiting = 1
         while waiting:
-            tileclicked2move = self.update(screen,clock)
+            tileclicked2move = self.update(screen, clock)
             if tileclicked2move in adj_tiles:
                 waiting = 0
                 tileclicked2move.initialize_army(tileclicked.army)
             elif tileclicked2move in army_present:
-                waiting = Combinearmymenu(tileclicked,tileclicked2move).update(screen,clock)
+                tempmenu = Combinearmymenu(tileclicked, tileclicked2move)
+                waiting = tempmenu.update(screen, clock)
                 if not waiting:
                     tileclicked2move.army += tileclicked.army
         for i in adj_tiles:
@@ -96,40 +101,47 @@ class Tilemap(pttt.Tilemap):
             i.toggle_shade('blue')
         tileclicked.remove_army()
 
+
 class Tilemenu(ptg.Menu):
-    def __init__(self,tileclicked):
+    def __init__(self, tileclicked):
         if tileclicked.army:
             header = ['Choose an Action']
-            buttons = [['View',lambda:4],['Move',lambda:5],['Return',lambda:1]]
+            buttons = [['View', lambda:4], ['Move', lambda:5],
+                       ['Return', lambda:1]]
         else:
             header = ['No Army Present']
-            buttons = [['Return',lambda:1]]
-        ptg.Menu.__init__(self,(500,400),(255,69,0),header,buttons)
-        ptg.Menu.set_offset(self,(400,300),mid = 'c')
+            buttons = [['Return', lambda:1]]
+        ptg.Menu.__init__(self, (500, 400), (255, 69, 0), header, buttons)
+        ptg.Menu.set_offset(self, (400, 300), mid='c')
+
 
 class Armyview(ptg.Menu):
-    def __init__(self,army):
+    def __init__(self, army):
         header = ['Army View']
         buttons = []
-        ptg.Menu.__init__(self,(500,400),(255,69,0),header,buttons)
-        self.buttonlist += [ptg.Button(0,'Return',(250,350),True,self.image,func = lambda:2)]
-        self.buttonlist += [ptg.Button(0,'Split',(250,300),True,self.image,func = lambda:6)]
+        ptg.Menu.__init__(self, (500, 400), (255, 69, 0), header, buttons)
+        self.buttonlist += [ptg.Button(0, 'Return', (250, 350), True,
+                                       self.image, func=lambda:2)]
+        self.buttonlist += [ptg.Button(0, 'Split', (250, 300), True,
+                                       self.image, func=lambda:6)]
 
         for i in army:
-            self.image.blit(i.tile.image,((army.index(i)+1)*12+army.index(i)*150,100))
+            self.image.blit(i.tile.image,
+                            ((army.index(i)+1)*12+army.index(i)*150, 100))
 
-        ptg.Menu.set_offset(self,(400,300),mid = 'c')
+        ptg.Menu.set_offset(self, (400, 300), mid='c')
+
 
 class Combinearmymenu(ptg.Menu):
-    def __init__(self,original_tile,final_tile):
+    def __init__(self, original_tile, final_tile):
         if len(original_tile.army) + len(final_tile.army) > 3:
             header = ['Too many monsters', 'present to combine armies']
-            buttons = [['Return',lambda:1]]
+            buttons = [['Return', lambda:1]]
         else:
             header = ['Would you like to combine armies?']
-            buttons = [['yes',lambda:0],['no',lambda:1]]
-        ptg.Menu.__init__(self,(500,400),(255,69,0),header,buttons)
-        ptg.Menu.set_offset(self,(400,300),mid = 'c')
+            buttons = [['yes', lambda:0], ['no', lambda:1]]
+        ptg.Menu.__init__(self, (500, 400), (255, 69, 0), header, buttons)
+        ptg.Menu.set_offset(self, (400, 300), mid='c')
 
 
 class Splitarmymenu(ptg.Menu):
@@ -177,38 +189,43 @@ class Splitarmyerror(ptg.Menu):
         ptg.Menu.__init__(self, (400, 300), (60, 69, 255), header, buttons)
         ptg.Menu.set_offset(self, (400, 300), mid='c')
 
+
 class Main(object):
     def __init__(self):
         self.progress = 1
         self.clock = pygame.time.Clock()
-        self.tilelist = pttt.Tilelist([[Tile() for i in range(0,4)],
-                                       [Tile() for i in range(0,4)],
-                                       [Tile() for i in range(0,4)],
-                                       [Tile() for i in range(0,4)]])
+        self.tilelist = pttt.Tilelist([[Tile() for i in range(0, 4)],
+                                       [Tile() for i in range(0, 4)],
+                                       [Tile() for i in range(0, 4)],
+                                       [Tile() for i in range(0, 4)]])
 
-
-        self.tilelist[0][1].initialize_army(army([monster(0),monster(1)]))
-        self.tilelist[2][2].initialize_army(army([monster(3),monster(4),monster(5)]))
+        self.tilelist[0][1].initialize_army(army([monster(0), monster(1)]))
+        self.tilelist[2][2].initialize_army(army([monster(3), monster(4),
+                                                  monster(5)]))
         self.tilelist[0][2].initialize_army(army([monster(2)]))
 
-    def update(self,screen):
+    def update(self, screen):
         while True:
             if self.progress == 1:
-                tileclicked = Tilemap(self.tilelist).update(screen,self.clock)
+                tileclicked = Tilemap(self.tilelist).update(screen, self.clock)
                 self.progress = 2
             elif self.progress == 2:
-                self.progress = Tilemenu(tileclicked).update(screen,self.clock)
+                self.progress = Tilemenu(tileclicked).update(screen, self.clock)
             elif self.progress == 4:
-                self.progress = Armyview(tileclicked.army).update(screen,self.clock)
+                self.progress = Armyview(tileclicked.army).update(screen,
+                                                                  self.clock)
                 if self.progress == 6:
                     if len(tileclicked.army) == 1:
                         self.progress = Splitarmyerror(1).update(screen,
                                                                  self.clock)
             elif self.progress == 5:
-                Tilemap(self.tilelist).move_update(tileclicked,screen,self.clock)
+                Tilemap(self.tilelist).move_update(tileclicked,
+                                                   screen, self.clock)
                 self.progress = 1
             elif self.progress == 6:
-                [self.progress, self.split_status] = Splitarmymenu(tileclicked.army).update(screen,self.clock)
+                tempmenu = Splitarmymenu(tileclicked.army)
+                [self.progress, self.split_status] = tempmenu.update(screen,
+                                                                     self.clock)
                 if self.progress == 4:
                     for i in tileclicked.army:
                         if i.tile.shades['blue'][0]:
@@ -227,7 +244,7 @@ class Main(object):
                     for i in tileclicked.army:
                         i.tile(Splitarmymenu(tileclicked.army))
                     Tilemap(self.tilelist).move_update(tileclicked, screen,
-                                                    self.clock)
+                                                       self.clock)
                     tileclicked.initialize_army(temp_army)
                     self.progress = 1
 
