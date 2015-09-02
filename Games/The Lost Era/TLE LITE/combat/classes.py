286cbd5d05ce9ab353abea9fbf7f3a4d695496cf
jbm950 <jmilam343@gmail.com>
2015-09-01 20:42:57 -0400

    lte lite work

---
+++ Games/The Lost Era/TLE LITE/combat/classes.py
@@ -0,0 +1,21 @@
+# ------------------------------------------------------------------------------
+# Name:        classes.py
+# Purpose:     This file will contain the information of the classes that the
+#              units can have. (ex. Warrior, Mage)
+#
+# Author:      James
+#
+# Created:     01/09/2015
+# Copyright:   (c) James 2015
+# Licence:     <your licence>
+# ------------------------------------------------------------------------------
+# !/usr/bin/env python
+
+
+class Warrior:
+    def __init__(self):
+        pass
+
+class Mage:
+    def __init__(self):
+        pass
---
+++ Games/The Lost Era/TLE LITE/combat/skills.py
@@ -0,0 +1,11 @@
+# ------------------------------------------------------------------------------
+# Name:        skills.py
+# Purpose:     This file will contain the data for the skills/spells/special
+#              abilities of the different units
+# Author:      James
+#
+# Created:     01/09/2015
+# Copyright:   (c) James 2015
+# Licence:     <your licence>
+# ------------------------------------------------------------------------------
+# !/usr/bin/env python
