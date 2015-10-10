8312262f8fa6e846c4e5845cdc2acae4811b775a
jbm950 <jmilam343@gmail.com>
2015-10-10 12:37:00 -0400

    added to simple c program

--- c_practice/first_c_program/main.c
+++ c_practice/first_c_program/main.c
@@ -2,8 +2,12 @@
 
 int main(){
     
+     int age = 23;
+     char name[14] = "Brandon Milam";
+
      printf("\nHello World\n\n");
      printf("Second line of text\n\n");
+     printf("My name is %s and I am %d years old\n\n", name, age);
 
      return 0;
 
